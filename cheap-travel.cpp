f23f57c70f01a84a5c0058b5176f40358cbc785b
apgoutham <apgoutham3@gmail.com>
2021-06-03 05:24:32 +0530

    running successfully

---
+++ cheap-travel.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,m,a,b;
+	cin >> n >> m >> a >> b;
+	int cost_wo_m = n*a;
+	int cost_w_m = (n/m)*b+(n%m)*a;
+	int cost_w_m_only = ((n/m)+1)*b;
+	cout << min(min(cost_wo_m, cost_w_m), cost_w_m_only) << endl;
+	return 0;
+}
